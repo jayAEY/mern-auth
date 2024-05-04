adeee9b6dc505b6446f88d963f63f403077f8096
jayAEY <106685702+jayAEY@users.noreply.github.com>
2024-05-04 08:46:19 -0700

    updated index.js

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://my-mern-auth.vercel.app/", "http://localhost:5173"],
+    origin: ["https://my-mern-auth.vercel.app", "http://localhost:5173"],
     methods: ["GET", "POST"],
     credentials: true,
   })
