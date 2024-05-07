4f9360176347df1c4887684d92f1e79820373634
jayAEY <106685702+jayAEY@users.noreply.github.com>
2024-05-06 18:57:45 -0700

    update index.js

--- api/index.js
+++ api/index.js
@@ -15,8 +15,8 @@ const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
 // const jwt, { decode } = "jsonwebtoken";
-// const bcrypt = require("bcrypt");
-// const { UserModel } = "./models/Users.js";
+const bcrypt = require("bcrypt");
+const { UserModel } = "./models/Users.js";
 
 // generate key = node -e "console.log(require('crypto').randomBytes(32).toString('hex'))"
 
@@ -66,56 +66,56 @@ app.get("/api/test", (req, res) => {
 //   }
 // });
 
-// app.post("/api/login", async (req, res) => {
-//   const { email, password } = req.body;
-//   try {
-//     let user = await UserModel.findOne({ email });
-//     if (user) {
-//       const validatedPassword = await bcrypt.compare(password, user.password);
-//       if (!validatedPassword) {
-//         res.send("Wrong Password");
-//       } else {
-//         const token = jwt.sign({ email }, process.env.JWT_KEY);
-//         res.cookie("token", token, {
-//           httpOnly: true,
-//           secure: true,
-//           sameSite: "None",
-//         });
-//         return res.json({ login: true });
-//       }
-//     } else {
-//       res.send("No user found");
-//     }
-//   } catch (err) {
-//     console.log("err");
-//     return res.send(err);
-//   }
-// });
+app.post("/api/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    let user = await UserModel.findOne({ email });
+    if (user) {
+      const validatedPassword = await bcrypt.compare(password, user.password);
+      if (!validatedPassword) {
+        res.send("Wrong Password");
+      } else {
+        const token = jwt.sign({ email }, process.env.JWT_KEY);
+        res.cookie("token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "None",
+        });
+        return res.json({ login: true });
+      }
+    } else {
+      res.send("No user found");
+    }
+  } catch (err) {
+    console.log("err");
+    return res.send(err);
+  }
+});
 
-// const verifyUser = (req, res, next) => {
-//   const token = req.cookies.token;
-//   if (!token) {
-//     res.json({ login: false });
-//   } else {
-//     jwt.verify(token, process.env.JWT_KEY, (err, decoded) => {
-//       if (err) {
-//         return res.json({ message: "Invalid Token" });
-//       } else {
-//         req.email = decoded.email;
-//         next();
-//       }
-//     });
-//   }
-// };
+const verifyUser = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) {
+    res.json({ login: false });
+  } else {
+    jwt.verify(token, process.env.JWT_KEY, (err, decoded) => {
+      if (err) {
+        return res.json({ message: "Invalid Token" });
+      } else {
+        req.email = decoded.email;
+        next();
+      }
+    });
+  }
+};
 
-// app.get("/api/verify", verifyUser, (req, res) => {
-//   return res.json({ login: true, email: req.email });
-// });
+app.get("/api/verify", verifyUser, (req, res) => {
+  return res.json({ login: true, email: req.email });
+});
 
-// app.get("/api/logout", (req, res) => {
-//   res.clearCookie("token");
-//   return res.json({ logout: true });
-// });
+app.get("/api/logout", (req, res) => {
+  res.clearCookie("token");
+  return res.json({ logout: true });
+});
 
 process.env.PORT &&
   app.listen(process.env.PORT, () => {
