5f4302f50d90d18565c72d16bcfc6ec15b10f399
jayAEY <106685702+jayAEY@users.noreply.github.com>
2024-05-11 14:24:19 -0700

    update index.js

--- api/index.js
+++ api/index.js
@@ -18,6 +18,7 @@ app.use(
     origin: [
       "https://2nd-mern-auth-frontend.vercel.app",
       "http://localhost:5173",
+      "*",
     ],
     methods: ["GET", "POST"],
     credentials: true,
