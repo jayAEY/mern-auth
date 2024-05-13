3f3e882509bae278aa8053481b7f0a14d0effc2f
jayAEY <106685702+jayAEY@users.noreply.github.com>
2024-05-13 14:48:00 -0700

    changed bcrypt to bcryptjs

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
 // const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const UserModel = require("./models/Users.js");
 
 // generate key = node -e "console.log(require('crypto').randomBytes(32).toString('hex'))"
@@ -17,13 +18,13 @@ app.use(
     origin: [
       "https://2nd-mern-auth-frontend.vercel.app",
       "http://localhost:5173",
-      "*",
+      // "*",
     ],
     methods: ["GET", "POST"],
     credentials: true,
   })
 );
-// app.use(cookieParser());
+app.use(cookieParser());
 
 const connect = async () => {
   try {
@@ -44,74 +45,74 @@ app.get("/api/test", (req, res) => {
   res.json({ body: Date() });
 });
 
-// app.post("/api/register", async (req, res) => {
-//   const { email, password } = req.body;
-//   try {
-//     const user = await UserModel.findOne({ email });
-//     if (user) {
-//       return res.send("User already exists");
-//     } else {
-//       const hashPassword = await bcrypt.hash(password, 10);
-//       const newUser = new UserModel({ email, password: hashPassword });
-//       await newUser.save();
-//       return res.send(`${email} is now registered`);
-//     }
-//   } catch (err) {
-//     console.log(err);
-//     return res.send(err);
-//   }
-// });
+app.post("/api/register", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await UserModel.findOne({ email });
+    if (user) {
+      return res.send("User already exists");
+    } else {
+      const hashPassword = await bcrypt.hash(password, 10);
+      const newUser = new UserModel({ email, password: hashPassword });
+      await newUser.save();
+      return res.send(`${email} is now registered`);
+    }
+  } catch (err) {
+    console.log(err);
+    return res.send(err);
+  }
+});
 
-// app.post("/api/login", async (req, res) => {
-//   const { email, password } = req.body;
-//   try {
-//     let user = await UserModel.findOne({ email });
-//     if (user) {
-//       const validatedPassword = await bcrypt.compare(password, user.password);
-//       if (!validatedPassword) {
-//         res.send("Wrong Password");
-//       } else {
-//         const token = jwt.sign({ email }, process.env.JWT_KEY);
-//         res.cookie("token", token, {
-//           httpOnly: true,
-//           secure: true,
-//           sameSite: "None",
-//         });
-//         return res.json({ login: true });
-//       }
-//     } else {
-//       res.send("No user found");
-//     }
-//   } catch (err) {
-//     console.log(err);
-//     return res.json(err);
-//   }
-// });
+app.post("/api/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    let user = await UserModel.findOne({ email });
+    if (user) {
+      const validatedPassword = await bcrypt.compare(password, user.password);
+      if (!validatedPassword) {
+        res.send("Wrong Password");
+      } else {
+        const token = jwt.sign({ email }, process.env.JWT_KEY);
+        res.cookie("token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "None",
+        });
+        return res.json({ login: true });
+      }
+    } else {
+      res.send("No user found");
+    }
+  } catch (err) {
+    console.log(err);
+    return res.json(err);
+  }
+});
 
-// const verifyUser = (req, res, next) => {
-//   const token = req.cookies.token;
-//   if (!token) {
-//     res.json({ login: false });
-//   } else {
-//     jwt.verify(token, process.env.JWT_KEY, (err, decoded) => {
-//       if (err) {
-//         return res.json({ message: "Invalid Token" });
-//       } else {
-//         req.email = decoded.email;
-//         next();
-//       }
-//     });
-//   }
-// };
+const verifyUser = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) {
+    res.json({ login: false });
+  } else {
+    jwt.verify(token, process.env.JWT_KEY, (err, decoded) => {
+      if (err) {
+        return res.json({ message: "Invalid Token" });
+      } else {
+        req.email = decoded.email;
+        next();
+      }
+    });
+  }
+};
 
-// app.get("/api/verify", verifyUser, (req, res) => {
-//   return res.json({ login: true, email: req.email });
-// });
+app.get("/api/verify", verifyUser, (req, res) => {
+  return res.json({ login: true, email: req.email });
+});
 
-// app.get("/api/logout", (req, res) => {
-//   res.clearCookie("token");
-//   return res.json({ logout: true });
-// });
+app.get("/api/logout", (req, res) => {
+  res.clearCookie("token");
+  return res.json({ logout: true });
+});
 
 process.env.PORT &&
   app.listen(process.env.PORT, () => {
